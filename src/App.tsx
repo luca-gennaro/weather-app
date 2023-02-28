adc20366a8187d0b48798a78cd6d8d0929d7f3b2
Luca Gennaro <lucagennaro91@gmail.com>
2023-02-28 16:43:57 +0100

    search ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 const App = () => {
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+      <section className="w-full md:max-w-lg p-4 flex flex-col text-center items-center justify-center md:p-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
+        <h1 className="text-4xl font-thin">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+
+        <p className="mt-2 text-sm">
+          Enter below a place you want to know the weather of and select an option from the dropdown
+        </p>
+
+        <div className="flex mt-10 md:mt-4">
+          <input
+            type="text"
+            value={""}
+            className="px-2 py-1 rounded-l-md border-2 border-white"
+          />
+  
+          <button className="rounded-r-md border-2 border-zinc-100 text-zinc-100 hover:border-zinc-500 hover:text-zinc-500 px-2 py-1 cursor-pointer">
+            search
+          </button>
+        </div>
+      </section>
     </main>
   )
 }
