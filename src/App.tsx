f0f1dab6ca3bead67c3ccdd88b92db6060edfc22
Luca Gennaro <lucagennaro91@gmail.com>
2023-03-01 14:04:53 +0100

    split the code components and hooks in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,29 @@
-import { ChangeEvent, useEffect, useState } from "react"
-
-import { optionType } from "./types"
+import Search from "./components/Search"
+import useForecast from "./hooks/useForecast"
 
 const App = (): JSX.Element => {
 
-  const [term, setTerm] = useState<string>("")
-  const [options, setOptions] = useState<[]>([])
-  const [city, setCity] = useState<optionType | null>(null) 
-
-  const getSearchOptions = (value:string) => {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()},&limit=5&appid=${process.env.REACT_APP_API_KEY}`)
-    .then((res) => res.json())
-    .then((data) => setOptions(data))
-  }
-
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim()
-    setTerm(value)
-
-    if(value === "") return
-
-    getSearchOptions(value)
-  } 
-
-  const getForecast = (city: optionType) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`).then(res => res.json()).then(data => console.log({data})
-    )
-  }
-
-  const onSubmit = () => {
-    if(!city) return
-
-    getForecast(city)
-  }
-
-  const onOptionSelect = (option: optionType) => {
-    setCity(option);
-  }
-
-  useEffect(()=>{
-    if(city) {
-      setTerm(city.name)
-      setOptions([])
-    }
-  }, [city])
+  const {term,
+    options,
+    forecast,
+    onInputChange,
+    onSubmit,
+    onOptionSelect} = useForecast()
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      <section className="w-full md:max-w-lg p-4 flex flex-col text-center items-center justify-center md:p-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-zinc-700">
-        <h1 className="text-4xl font-thin">
-          Weather <span className="font-black">Forecast</span>
-        </h1>
+      {forecast ? (
+        "we have a forecast"
+      ) : (
+          <Search 
+       term = { term }
+       options = { options }
+       onInputChange = { onInputChange }
+       onOptionSelect = { onOptionSelect }
+       onSubmit = { onSubmit }
+      />
+      )}
 
-        <p className="mt-2 text-sm">
-          Enter below a place you want to know the weather of and select an option from the dropdown
-        </p>
-
-        <div className=" relative flex mt-10 md:mt-4">
-          <input
-            type="text"
-            value={term}
-            onChange={onInputChange}
-            className="px-2 py-1 rounded-l-md border-2 border-white"
-          />
-
-          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
-            {options.map((option: optionType, index:number) => (
-              <li key={`${option.name}-${index}`}>
-                <button className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer" onClick={() => onOptionSelect(option)}>
-                  {option.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-  
-          <button className="rounded-r-md border-2 border-zinc-100 text-zinc-100 hover:border-zinc-500 hover:text-zinc-500 px-2 py-1 cursor-pointer" onClick={onSubmit}>
-            search
-          </button>
-        </div>
-      </section>
     </main>
   )
 }
